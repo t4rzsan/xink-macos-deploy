2dc0489eebb26f7670f3a58ed10a871f5c14e5dd
Jakob Christensen <jakob.christensen@gmail.com>
2021-08-15 14:11:58 +0200

    Add script

---
+++ XinkDeploymentScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+defaults write com.emailsignature.Xink hideInfobox -int 0
+defaults write com.emailsignature.Xink hideQuitMailInfobox -int 0
+defaults write com.emailsignature.Xink isUsingICloudDrive -int 0
+defaults write com.emailsignature.Xink updateSignaturesAutomatically -int 0
+defaults write com.emailsignature.Xink updateSignaturesForMail -int 0
+defaults write com.emailsignature.Xink updateSignaturesForOutlook -int 1
+defaults write com.emailsignature.Xink userName -string "$(whoami)@INSERTYOUREMAILDOMAIN.COM"
+defaults write com.emailsignature.Xink domainToken -string "INSERT YOUR XINK DOMAIN TOKEN HERE"
