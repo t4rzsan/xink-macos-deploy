80fb1422630f0908d2761d03310e13225d462f8b
Jakob Christensen <t4rzsan@users.noreply.github.com>
2021-08-17 08:05:07 +0200

    Add script for putting Xink in Login Items

--- XinkDeploymentScript.sh
+++ XinkDeploymentScript.sh
@@ -7,3 +7,5 @@ defaults write com.emailsignature.Xink updateSignaturesForMail -int 0
 defaults write com.emailsignature.Xink updateSignaturesForOutlook -int 1
 defaults write com.emailsignature.Xink userName -string "$(whoami)@INSERTYOUREMAILDOMAIN.COM"
 defaults write com.emailsignature.Xink domainToken -string "INSERT YOUR XINK DOMAIN TOKEN HERE"
+
+osascript -e 'tell application "System Events" to make login item at end with properties {name:"Xink", path:"/Applications/Xink.app", hidden:false}'
