cb44e475c44d5cb367953979ffec4cfdd0822fd8
Jakob Christensen <t4rzsan@users.noreply.github.com>
2022-03-18 20:41:21 +0100

    Add information on "emailDomain" setting

--- XinkDeploymentScript.sh
+++ XinkDeploymentScript.sh
@@ -10,6 +10,9 @@ defaults write com.emailsignature.Xink updateSignaturesAutomatically -int 0
 defaults write com.emailsignature.Xink updateSignaturesForMail -int 0
 defaults write com.emailsignature.Xink updateSignaturesForOutlook -int 1
 defaults write com.emailsignature.Xink userName -string "$(whoami)@INSERTYOUREMAILDOMAIN.COM"
+# If $(whoami) does not give you the right email address, you can set the "emailDomain" setting instead.
+# Xink will use the domain to find the correct email from registered accounts in either Apple Mail or Microsoft Outlook.
+# default write com.emailsignature.Xink emailDomain -string "acme.com" 
 defaults write com.emailsignature.Xink domainToken -string "INSERT YOUR XINK DOMAIN TOKEN HERE"
 
 osascript -e 'tell application "System Events" to make login item at end with properties {name:"Xink", path:"/Applications/Xink.app", hidden:false}'
